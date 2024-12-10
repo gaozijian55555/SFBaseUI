b0e907a8f668c0a1005bce3fcc05195485902dcf
高子健 <448916193@qq.com>
2024-12-10 16:30:31 +0800

    public

--- SFBaseUI/Classes/BaseNavigationController.swift
+++ SFBaseUI/Classes/BaseNavigationController.swift
@@ -7,25 +7,25 @@
 
 import UIKit
 
-open class BaseNavigationController : UINavigationController {
+public class BaseNavigationController : UINavigationController {
     
-    open override init(rootViewController: UIViewController) {
+    public override init(rootViewController: UIViewController) {
         super.init(rootViewController: rootViewController)
         self.modalPresentationStyle = .fullScreen
     }
     
-    open required init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    open override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor.white
         self.interactivePopGestureRecognizer?.delegate = self
     }
     
-    open override func viewWillAppear(_ animated: Bool) {
+    public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.setNavigationBarHidden(true, animated: false)
         self.navigationBar.isTranslucent = false
